3339460b8e51494ad19ab34c7552445621145809
colorisland <chaewonstudy3@gmail.com>
2024-10-21 00:20:17 +0900

    FEAT: 첫 commit

--- src/main/java/hello/itemservice/domain/ItemRepository.java
+++ src/main/java/hello/itemservice/domain/ItemRepository.java
@@ -32,4 +32,8 @@ public class ItemRepository {
         findItem.setPrice(updateParam.getPrice());
         findItem.setQuantity(updateParam.getQuantity());
     }
+
+    public void clearStore() {
+        store.clear();
+    }
 }
--- src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -1,4 +1,44 @@
 package hello.itemservice.domain;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class ItemRepositoryTest {
+    ItemRepository itemRepository = new ItemRepository();
+
+    @AfterEach
+    void afterEach() {
+        itemRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        // given
+        Item item = new Item("lip tint", 5000, 5);
+
+        // when
+        Item savedItem = itemRepository.save(item);
+
+        // then
+        Item findItem = itemRepository.findById(savedItem.getId());
+        assertThat(findItem).isEqualTo(savedItem);
+    }
+
+    @Test
+    void findAll() {
+        Item item1 = new Item("cushion21", 5000, 5);
+        Item item2 = new Item("cushion23", 5000, 5);
+
+        itemRepository.save(item1);
+        itemRepository.save(item2);
+
+        List<Item> items = itemRepository.findAll();
+        assertThat(items.size()).isEqualTo(2);
+        assertThat(items).contains(item1, item2);
+    }
 }
